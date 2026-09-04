8065890b029e102b24f5b13f153496a322e764d1
graynaud-oss <g.raynaud@ermas.fr>
2026-09-04 08:52:41 +0200

    feat: add sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'base-engins-v1';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './app-icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.hostname === 'docs.google.com') return;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    return;
+  }
+  if (url.origin === self.location.origin) {
+    event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  }
+});
+
