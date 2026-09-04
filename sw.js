683c7359d5466e6c052763430fa6c5a9257362be
graynaud-oss <g.raynaud@ermas.fr>
2026-09-04 13:30:55 +0200

    chore: refresh tractor icon cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'base-engins-v3';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './app-icon.svg'];
+const CACHE_NAME = 'base-engins-v5';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
