39547aeda7f55de3ddc7e72dac4b382c9c69b5ac
graynaud-oss <g.raynaud@ermas.fr>
2026-09-04 09:03:35 +0200

    fix: expose desktop installation and refresh sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'base-engins-v1';
+const CACHE_NAME = 'base-engins-v2';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './app-icon.svg'];
 
 self.addEventListener('install', event => {
